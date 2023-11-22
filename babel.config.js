79138a907045b2dada65a32c8e56c700dab35f7d
tjupp <travisjupp@gmail.com>
2023-11-22 09:28:03 -0700

    dev deps

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
-export const presets = [
-  '@babel/preset-env',
-  ['@babel/preset-react', { runtime: 'automatic' }],
-];
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', {runtime: 'automatic'}],
+  ],
+};
