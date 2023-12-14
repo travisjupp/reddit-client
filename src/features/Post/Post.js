f35b950a37f7812638ba6c19911774d230b1acd6
tjupp <travisjupp@gmail.com>
2023-12-14 07:36:21 -0700

    setup mochajs for testing

--- src/features/Post/Post.js
+++ src/features/Post/Post.js
@@ -12,7 +12,7 @@ function Post(props) {
         <>
             <Container fluid className='justify-content-start'>
                 <Row>
-                    <Col xs={3} style={{border: "solid 1px red"}}>Column 1</Col>
+                    <Col xs={3} style={{ border: "solid 1px red" }}>Column 1</Col>
                     <Col>
                         <Card >
                             <Card.Img variant="top" data-src="holder.js/100x50?auto=yes&textmode=exact&theme=industrial" />
@@ -24,15 +24,15 @@ function Post(props) {
                                 <Avatar name='Jane' className='d-none d-lg-block d-xl-none'>lg breakpoint</Avatar>
                                 <Avatar name='Val' className='d-block d-sm-none'>xs breakpoint</Avatar>
                                 <Card.Text>Card.Text</Card.Text>
-                    
-                                      <button type="button" class="btn btn-primary position-relative">
-                        Inbox
-                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                          99+
-                          <span class="visually-hidden">unread messages</span>
-                        </span>
-                                      </button>
-                                {/* --bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem */}
+
+                                <button type="button" className="btn btn-primary position-relative">
+                                    Inbox
+                                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                        99+
+                                        <span className="visually-hidden">unread messages</span>
+                                    </span>
+                                </button>
+
                             </Card.Body>
                         </Card>
                     </Col>
