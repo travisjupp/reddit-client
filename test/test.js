093bfb185c4e85ef68bcb8ef853ad654ff4fb893
tjupp <travisjupp@gmail.com>
2023-12-15 06:46:37 -0700

    import chai for assertions, act to test if component is rendered and mounted. Build test-hooks to setup and teardown rootContainer for Avatar component. Assert that img src is svg

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-var assert = require('assert');
+// var assert = require('assert');
+import assert from 'node:assert/strict';
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
