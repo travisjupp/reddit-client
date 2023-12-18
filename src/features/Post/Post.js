9bb8ba0283f2e54574cc59db3b4b3828cf78da42
tjupp <travisjupp@gmail.com>
2023-12-18 11:48:42 -0700

    add extensions to imports for testign

--- src/features/Post/Post.js
+++ src/features/Post/Post.js
@@ -3,7 +3,7 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Avatar from "../Avatar/Avatar";
+import Avatar from "../Avatar/Avatar.js";
 import 'holderjs';
 
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 
