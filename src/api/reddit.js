b28d86bc1af2a8ea8744082ddb86eaf6bd5e29c9
tjupp <travisjupp@gmail.com>
2023-12-23 08:23:08 -0700

    fetch subreddit titles

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -1,14 +1,24 @@
- // filter popular.json
+// filter popular.json
+const apiRoot = 'http://localhost:8000';
+const subredditsPathName = '/subreddits/1';
 
- export const getSubredditTitles = async () => {
-   //  const response = await fetch('http://localhost:3000/data/popular.json');
-   //  const response = await fetch('https://www.reddit.com/subreddits.json');
-   //  const response = await fetch('http://localhost:3000/data/subreddits.json');
-   //  const response = await fetch('http://localhost:3000/posts/1/comments');
-    const response = await fetch('http://localhost:3000/posts');
+export const getSubredditTitles = async () => {
+  try {
+    
+    //  const response = await fetch('https://www.reddit.com/subreddits.json');
+    // const response = await fetch('http://localhost:8000/subreddits/1');
+    const response = await fetch(`${apiRoot}${subredditsPathName}`);
     const json = await response.json();
     console.log(json);
- }
+    console.log(json.data.children[0].data.title);
+    console.log(json.data.children[0].data.url);
+    console.log(json.data.children[1].data.title);
+    console.log(json.data.children[1].data.url);
+  } catch (e) {
+    console.log('error', e);
+    console.log('error message', e.message);
+  }
+}
 
 //  console.log('./popular.json'.json());
 //  fetch('http://localhost:3000/data/popular.json')
