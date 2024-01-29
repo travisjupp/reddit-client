505d7c7df2d0ad48947550f619a884181b5395ef
tjupp <travisjupp@gmail.com>
2024-01-29 07:23:24 -0700

    remove unused imports

--- src/features/Main/Main.jsx
+++ src/features/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { apiRoot, getSubredditPosts } from '../api/reddit';
 import Post from '../Post/Post';
@@ -12,29 +12,35 @@ function Main(props) {
         dispatch(getSubredditPosts('MapPorn'));
     }, [dispatch]);
 
-    const posts = useSelector(selectSubredditPosts);
-    const status = useSelector(selectSubredditPostsStatus);
-    console.log('posts', posts);
-    // console.log('posts[0]',posts[0]);
-    console.log('status', status);
-    while (status !== 'succeeded') {
-        return <StatusLoader />
-    }
-    return (
-        <>
-            <h1>r/{posts[0].data.subreddit}</h1>
-            {posts.map(post => {
-                return <Post
-                    key={post.data.id}
-                    postTitle={post.data.title}
-                    postImgSrc={post.data.url}
-                    postText={post.data.selftext}
-                    altText={`r/${post.data.subreddit} - ${post.data.title}`}
-                // postText={'#####################################################################################################################################################################################################################################################'}
-                />
-            })}
-        </>
-    )
-}
 
-export default Main;
+        const posts = useSelector(selectSubredditPosts);
+        const status = useSelector(selectSubredditPostsStatus);
+        console.log('posts', posts);
+        // console.log('posts[0]',posts[0]);
+        console.log('status', status);
+        while (status !== 'succeeded') {
+            return <StatusLoader />
+        }
+        const validatePostImgURL = (url) => {
+            return url.match(/jpeg|jpg|png/i) ? url : null;
+        }
+
+        return (
+            <>
+                <h1>r/{posts[0].data.subreddit}</h1>
+
+                {posts.map(post => {
+                    return <Post
+                        key={post.data.id}
+                        postTitle={post.data.title}
+                        postImgSrc={validatePostImgURL(post.data.url)}
+                        postText={post.data.selftext}
+                        altText={`r/${post.data.subreddit} - ${post.data.title}`}
+                    // postText={'#####################################################################################################################################################################################################################################################'}
+                    />
+                })}
+            </>
+        )
+    }
+
+    export default Main;
--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -1,8 +1,5 @@
 import React, {useEffect} from 'react';
 import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import Avatar from "../Avatar/Avatar";
 // import 'holderjs';
 
