884662b9f7326b1b36a023585ebd3baeb72b49c4
tjupp <travisjupp@gmail.com>
2024-03-15 06:43:23 -0600

    render comments as markdown

--- src/features/Comment/Comment.jsx
+++ src/features/Comment/Comment.jsx
@@ -1,51 +1,72 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Card, Stack } from "react-bootstrap";
+import { Card, Stack, Button } from "react-bootstrap";
 import Avatar from "../Avatar/Avatar";
+import StatusLoader from '../../components/StatusLoader/StatusLoader';
 import { selectUserAvatar } from "../../store/subredditPostsSlice";
 import { getUserAvatar } from "../api/reddit";
 import validateAvatarImgURL from '../../utils/validateImgURL.js';
+import formatPostText from "../../utils/formatPostText.js";
+import Markdown from "react-markdown";
+import rehypeRaw from 'rehype-raw';
+
+
+import { selectSubredditCommentsStatus, selectSubredditCommentsError } from "../../store/subredditCommentsSlice.js";
 
 
 function Comment(props) {
-    const { comment, show } = props;
+    const {
+        commentAuthor,
+        commentDate,
+        commentText,
+        commentTextHtml,
+        show,
+    } = props;
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(getUserAvatar(comment.data.author));
-
-    }, [dispatch, comment.data.author])
+        dispatch(getUserAvatar(commentAuthor));
+    }, [dispatch, commentAuthor])
     const avatar = useSelector(selectUserAvatar);
 
-
-    return (
-
-        <>
-            <Card className={show ? null : 'd-none'}>
-                <Card.Body>
-                    <Card.Title>
-
-                        <Stack direction="horizontal" gap={3}>
-                            <div className="p-2">
-                                <Avatar
-                                    name={comment.data.author}
-                                    src={validateAvatarImgURL(avatar[comment.data.author])}
-                                /> {comment.data.author}
-                            </div>
+    const commentsStatus = useSelector(selectSubredditCommentsStatus);
+    const commentsErrorState = useSelector(selectSubredditCommentsError);
 
 
+    while (commentsStatus === 'loading') {
+        return <StatusLoader />
+    }
+    if (commentsStatus === 'succeeded') {
+        return (
+            <>
+                <Card className={show ? null : 'd-none'}>
+                    <Card.Body>
+                        <Card.Title>
 
-                            <div className="p-2 ms-auto">{new Date(comment.data.created_utc * 1000).toString()}</div>
-                        </Stack>
+                            <Stack direction="horizontal" gap={3}>
+                                <div className="p-2">
+                                    <Avatar
+                                        name={commentAuthor}
+                                        src={validateAvatarImgURL(avatar[commentAuthor])}
+                                    /> {commentAuthor}
+                                </div>
 
 
-                    </Card.Title>
-                    <Card.Text>
-                        {comment.data.body}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </>
-    )
+
+                                <div className="p-2 ms-auto">{new Date(commentDate * 1000).toString()}</div>
+                            </Stack>
+
+
+                        </Card.Title>
+                        <Card.Text as='div'>{/* Render as 'div' to avoid <p> nesting; <p> cannot appear as a descendant of <p>. */}
+                            <Markdown rehypePlugins={[rehypeRaw]}>{formatPostText(commentTextHtml, commentText, 300)}</Markdown>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </>
+        )
+    }
+
+ 
 
 
 }
