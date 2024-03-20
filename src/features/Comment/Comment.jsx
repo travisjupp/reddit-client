17176c4c404bd24be2247f398b40e63c70cb713b
tjupp <travisjupp@gmail.com>
2024-03-20 07:01:13 -0600

    disable CSS-based showing of component; have a react-bootstrap "Transitions" component (Collapse or Fade) handle it

--- src/features/Comment/Comment.jsx
+++ src/features/Comment/Comment.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Card, Stack, Button } from "react-bootstrap";
+import { Card, Stack, Collapse } from "react-bootstrap";
 import Avatar from "../Avatar/Avatar";
 import StatusLoader from '../../components/StatusLoader/StatusLoader';
 import { selectUserAvatar } from "../../store/subredditPostsSlice";
@@ -11,7 +11,7 @@ import Markdown from "react-markdown";
 import rehypeRaw from 'rehype-raw';
 
 
-import { selectSubredditCommentsStatus, selectSubredditCommentsError } from "../../store/subredditCommentsSlice.js";
+import { selectSubredditCommentsStatus } from "../../store/subredditCommentsSlice.js";
 
 
 function Comment(props) {
@@ -29,44 +29,48 @@ function Comment(props) {
     const avatar = useSelector(selectUserAvatar);
 
     const commentsStatus = useSelector(selectSubredditCommentsStatus);
-    const commentsErrorState = useSelector(selectSubredditCommentsError);
 
 
-    while (commentsStatus === 'loading') {
-        return <StatusLoader />
-    }
+
+    // while (commentsStatus === 'loading') {
+    //     return <StatusLoader />
+    // }
     if (commentsStatus === 'succeeded') {
         return (
             <>
-                <Card className={show ? null : 'd-none'}>
-                    <Card.Body>
-                        <Card.Title>
+                {/* <Collapse in={show}> */}
+                    <Card
+                    //  className={show ? 'comment' : 'd-none comment'}
+                     >
+                        <Card.Body>
+                            <Card.Title>
 
-                            <Stack direction="horizontal" gap={3}>
-                                <div className="p-2">
-                                    <Avatar
-                                        name={commentAuthor}
-                                        src={validateAvatarImgURL(avatar[commentAuthor])}
-                                    /> {commentAuthor}
-                                </div>
-
-
-
-                                <div className="p-2 ms-auto">{new Date(commentDate * 1000).toString()}</div>
-                            </Stack>
+                                <Stack direction="horizontal" gap={3}>
+                                    <div className="p-2">
+                                        <Avatar
+                                            name={commentAuthor}
+                                            src={validateAvatarImgURL(avatar[commentAuthor])}
+                                        /> {commentAuthor}
+                                    </div>
 
 
-                        </Card.Title>
-                        <Card.Text as='div'>{/* Render as 'div' to avoid <p> nesting; <p> cannot appear as a descendant of <p>. */}
-                            <Markdown rehypePlugins={[rehypeRaw]}>{formatPostText(commentTextHtml, commentText, 300)}</Markdown>
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+
+                                    <div className="p-2 ms-auto">{new Date(commentDate * 1000).toString()}</div>
+                                </Stack>
+
+
+                            </Card.Title>
+                            <Card.Text as='div'>{/* Render as 'div' to avoid <p> nesting; <p> cannot appear as a descendant of <p>. */}
+                                <Markdown rehypePlugins={[rehypeRaw]}>{formatPostText(commentTextHtml, commentText, 300)}</Markdown>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                {/* </Collapse> */}
             </>
         )
     }
 
- 
+
 
 
 }
