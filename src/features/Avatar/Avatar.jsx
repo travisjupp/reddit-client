b7142a2f35a6ed1c7186d1127ae9c71358980c10
tjupp <travisjupp@gmail.com>
2024-07-05 12:29:46 -0600

    Pass a `size` param to Avatar components

--- src/features/Avatar/Avatar.jsx
+++ src/features/Avatar/Avatar.jsx
@@ -4,18 +4,18 @@ import { identicon } from '@dicebear/collection';
 
 // return avatar icon img or default to identicon
 function Avatar(props) {
-    const { name, src } = props;
+    const { name, src, size = 28 } = props;
     const avatar = useMemo(() => {
         return createAvatar(identicon, {
-            size: 28,
+            size: size,
             seed: name,
             rowColor: ["c4c4c4"],
             // ... other options
         }).toDataUriSync();
     }, [name]);
     // render avatar or identicon if src not provided
-    return !src ? <img src={avatar} alt="Avatar" /> :
-        <img src={src} alt="Avatar" width={28} height={28} />;
+    return !src ? <img src={avatar} alt="Avatar" style={{borderRadius: "50%"}} /> :
+        <img src={src} alt="Avatar" width={size} height={size} style={{borderRadius: "50%"}} />;
 }
 
 export default Avatar;
