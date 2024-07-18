29663d2492ab4712a6acc2465c48dfe620287807
tjupp <travisjupp@gmail.com>
2024-07-18 14:30:46 -0600

    Remove unused vars

--- src/utils/formatPostMedia.js
+++ src/utils/formatPostMedia.js
@@ -1,16 +1,16 @@
 // https://github.com/mathiasbynens/he
 // he (for “HTML entities”) is a robust HTML entity encoder/decoder written in JavaScript.
-import he from 'he';
-import parse from 'html-react-parser';
+// import he from 'he';
+// import parse from 'html-react-parser';
 
 // If post media exists, decode html-entites and render.
 const formatPostMedia = (postMedia, postMediaPreview) => {
     try {
         if (typeof postMedia.content === 'string') {
-            const postMediaDomElement = he.decode(postMedia.content);
+            // const postMediaDomElement = he.decode(postMedia.content);
             // => thumbnail media image component that opens a modal on click
             console.log('-->',postMediaPreview.images[0].source)
-            return <img src={postMediaPreview.images[0].source.url} />
+            return <img src={postMediaPreview.images[0].source.url} alt="Media Preview" />
             // return parse(postMediaDomElement);
         } else {
             return;
