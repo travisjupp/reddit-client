8c70ff644894cd59a37f6bf451b4397cc34511b5
tjupp <travisjupp@gmail.com>
2024-07-24 11:37:17 -0600

    Return post images if available
    The Media component will be used to return all post related media eg.
    media embeds, images, etc.. Is also responsible for rendering source
    sets (picture elements with source tags containing `srcset` attributes).

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -8,28 +8,78 @@ import parse from 'html-react-parser';
 
 // If post media exists, decode html-entites and render.
 const Media = ({postMedia}) => {
-    const {mediaEmbed,preview} = postMedia;
-    console.log('~>postMedia',postMedia);
-    console.log('~>mediaEmbed, preview',mediaEmbed, preview);
+    const {mediaEmbed, preview, isGallery, metadata, data} = postMedia;
+    console.log('~>postMedia', postMedia);
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
     try {
+        // check for image only
+        if (preview) {
+            // get preview resolutions
+            const resolutions = preview.images[0].resolutions;
+            // sort resolutions so browser doesn't 
+            // load lowest `<source width..` media query first
+            const sortedResolutions = [...resolutions].sort((a, b) => b.width - a.width);
+            const previewId = preview.images[0].id; // get preview id
+            const previewSource = preview.images[0].source; // get preview source
+            console.log('~>preview', preview, '\n~>isGallery', isGallery == true,
+                '\n~>resolutions', resolutions, '\n~>sortedResolutions', sortedResolutions);
+
+            return <picture key={previewId} id={previewId}>
+                {sortedResolutions.map(image => {
+                    return (
+                        <source
+                            srcset={he.decode(image.url)}
+                            media={`(min-width: ${image.width}px)`}
+                            width={image.width}
+                            height={image.height}
+                        />
+                    )
+                })
+                }
+                <img src={he.decode(previewSource.url)}
+                    width={previewSource.width}
+                    height={previewSource.height} alt="Post Image" />
+            </picture>
+        }
         // check for embedded media
+        console.log('mediaEmbed.content', mediaEmbed.content);
         if (typeof mediaEmbed.content === 'string') {
             const postMediaDomElement = he.decode(mediaEmbed.content);
-            // => thumbnail media image component that opens a modal on click
-            // console.log('he.decode', he.decode(postMediaPreview.images[0].source.url))
-            return (<> <img onClick={handleShow} src={he.decode(preview.images[0].source.url)} alt="Media Preview" />
-                <Modal show={show} onHide={handleClose} centered>
-                    <Modal.Header closeButton>
-                        {/* <Modal.Title>Modal heading</Modal.Title> */}
-                    </Modal.Header>
-                    <Modal.Body className="row-cols-1">{parse(postMediaDomElement)}</Modal.Body>
-                </Modal>
-            </>)
+            // => media preview image opens media source in modal on click
+            // build srcset from `preview.images[0].resolutions`
+            let resolutions = preview.images[0].resolutions;
+            console.log('~>resolutions', resolutions);
+
+            return (
+                <>
+                    <img onClick={handleShow} src={he.decode(preview.images[0].source.url)} alt="Media Preview" />
+                    <Modal show={show} onHide={handleClose} centered>
+                        <Modal.Header closeButton>
+                            {/* <Modal.Title>Modal heading</Modal.Title> */}
+                        </Modal.Header>
+                        <Modal.Body className="row-cols-1">{parse(postMediaDomElement)}</Modal.Body>
+                    </Modal>
+                </>
+            )
             // return parse(postMediaDomElement); // return parsed DOM node
         } else {
+            // check for gallery
+            if (isGallery) {
+                console.log('~>isGallery', isGallery, '\nmetadata', metadata, '\ndata', data);
+                for (const key in metadata) {
+                    console.log('~>image', metadata[key]);
+                    let image = metadata[key];
+
+                    console.log('~>metadata[key][]',)
+
+                    for (const imgKey in metadata[key]) {
+                        console.log('~>imgKey', imgKey, '=>', metadata[key][imgKey])
+                    }
+                }
+            }
             return;
         }
     } catch (e) {
