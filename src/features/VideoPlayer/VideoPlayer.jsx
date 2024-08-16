0edceb049834edbafd0332885fa78d59c3dfc238
tjupp <travisjupp@gmail.com>
2024-08-16 14:02:43 -0600

    Use the __RIGHT__ url this time
    Video wasn't loading due to the wrong url being used. Instead of the url
    passed to the component, we we're using the url from the `currentSrc`
    property of the DOM element (ref) which was coming up empty.

--- src/features/VideoPlayer/VideoPlayer.jsx
+++ src/features/VideoPlayer/VideoPlayer.jsx
@@ -2,17 +2,14 @@ import dashjs from "dashjs";
 import {useEffect, useRef} from "react";
 
 function VideoPlayer({redditVideoURL, postId}) {
-const ref = useRef(null);
+  const ref = useRef(null);
   useEffect(() => {
 
-   console.log('effect ran: dashjs\n ref:', ref);
-    if (!ref.current?.id
-      // || ref.current.currentSrc.match(/(blob:)/)
-    ) return;
-    const url = ref.current.currentSrc;
+    console.log('effect ran: dashjs\n ref:', ref);
+    if (!ref.current?.id) return;
     const player = dashjs.MediaPlayer().create();
     try {
-      player.initialize(ref.current, url, false);
+      player.initialize(ref.current, redditVideoURL, false);
       player.on(dashjs.MediaPlayer.events.CAN_PLAY, () => {console.log('CAN_PLAY')});
     } catch (e) {
       console.error('Init Video Error: ', e.message);
@@ -21,14 +18,14 @@ const ref = useRef(null);
       player.off(dashjs.MediaPlayer.events.CAN_PLAY, () => {console.log('CAN_PLAY')});
       player.destroy();
     }
-  }, [redditVideoURL]);
+  }, [redditVideoURL, ref]);
 
   return (
-                <>
-                    <video ref={ref} id={'video-' + postId} controls className="card-img-top">
-                        <source src={redditVideoURL} />
-                    </video>
-                </>
+    <>
+      <video ref={ref} id={'video-' + postId} controls className="card-img-top">
+        <source src={redditVideoURL} />
+      </video>
+    </>
   );
 }
 
