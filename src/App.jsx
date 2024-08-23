a26906bb73c9bdd9060e5467c1a9c33f370a1f76
tjupp <travisjupp@gmail.com>
2024-08-22 19:02:48 -0600

    Update styles: bg image

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
           </Col>
           {/* MAIN */}
           <Col as='main'>
+            <div className='bg'></div>
             <Main />
           </Col>
         </Row>
